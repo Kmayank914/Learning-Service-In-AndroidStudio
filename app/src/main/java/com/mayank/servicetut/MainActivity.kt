69338c88045fb7ce386ae94a98ed1cb37b4a5cf5
ServiceTutorial <mayank.10.panchi@gmail.com>
2024-09-25 15:00:20 +0530

    Intial Commit

---
+++ app/src/main/java/com/mayank/servicetut/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.mayank.servicetut
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var startBtn=findViewById<Button>(R.id.button)
+        var stopBtn=findViewById<Button>(R.id.button2)
+
+        startBtn.setOnClickListener {
+            var startServiceIntent=Intent(applicationContext,MyService::class.java)
+
+            startService(startServiceIntent)
+        }
+        stopBtn.setOnClickListener {
+
+            var stopServiceIntent=Intent(applicationContext,MyService::class.java)
+
+            stopService(stopServiceIntent)
+
+        }
+
+        var text=findViewById<TextView>(R.id.textView)
+    }
+}
---
+++ app/src/main/java/com/mayank/servicetut/MyService.kt
@@ -0,0 +1,38 @@
+package com.mayank.servicetut
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import android.provider.Settings
+
+class MyService : Service() {
+
+    // To play music we need media player
+    lateinit var mediaPlayer: MediaPlayer
+    override fun onBind(intent: Intent?): IBinder? {
+
+
+
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        mediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI)
+        mediaPlayer.isLooping
+
+        mediaPlayer.start()
+
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+
+
+        mediaPlayer.stop()
+    }
+}
